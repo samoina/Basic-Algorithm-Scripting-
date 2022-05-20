28ce2a55651d550d201867c649353ecc5ad1ed0a
Samoina <samoina.w@gmail.com>
2022-05-20 13:01:48 +0300

    DRY code for this solution

--- titlecase-sentence.js
+++ titlecase-sentence.js
@@ -1,23 +1,24 @@
 
-function titleCase(str) {
-  let wordArr=[],
-      newWordArr=[],
-      firstLetter,
-      newStr='';
+// function titleCase(str) {
+//   let wordArr=[],
+//       newWordArr=[],
+//       firstLetter,
+//       newStr='';
 
-  let strArr = str.toLowerCase().split(' ').map(function(word){
-    for(let i=0; i<word.length; i++){
-      wordArr = word.split('');
-      firstLetter = wordArr[0].toUpperCase();
-      wordArr.splice(0, 1, firstLetter);
-      newWordArr = wordArr.join('');
-    }
-    newStr = newStr + ' ' + newWordArr;
-  });
-  return newStr.slice(1);
-};
+//   let strArr = str.toLowerCase().split(' ').map(function(word){
+//     for(let i=0; i<word.length; i++){
+//       wordArr = word.split('');
+//       firstLetter = wordArr[0].toUpperCase();
+//       wordArr.splice(0, 1, firstLetter);
+//       newWordArr = wordArr.join('');
+//     }
+//     newStr = newStr + ' ' + newWordArr;
+   
+//   });
+//   return newStr.slice(1);
+// };
 
-console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"));
+// console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"));
 
 /*This task gave me some bit of a challenge. here's how I had approached it. The first thing would be to :
 1. convert the entire string to lowercase
@@ -29,3 +30,13 @@ console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"));
 NB: Because I wasnt sure how to get rid of the space, I sliced the first character.
 
 */
+
+
+//I worked through this solution with a friend and it is indeed DRY code. it takes the same approach as I had: convert the string to lowercase, split to an array, then loop over using the map method. Inside the map function, i take each word, and replace the first letter with its uppercase, then join the array. Far fewer variables.
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word){
+    return word.replace(word[0], word[0].toUpperCase());
+  }).join(' ');
+}
+
+console.log(titleCase("I'm a little tea pot"));
